96b626840a15149b00da9e6d00ec0b4c69271e7c
Bert Wesarg <bert.wesarg@tu-dresden.de>
2025-09-15 17:46:17 +0200

    Move RocmSensorType into class RocmSensor

--- include/rocm_sensor.hpp
+++ include/rocm_sensor.hpp
@@ -3,58 +3,60 @@
 
 #include <map>
 #include <rocm_smi/rocm_smi.h>
-enum class RocmSensorType
-{
-    INVALID,
-    SOCKET_POWER,
-    AVERAGE_POWER,
-    ENERGY_COUNT,
-    MEMORY_USAGE_VRAM,
-    MEMORY_USAGE_VIS_VRAM,
-    MEMORY_USAGE_GTT,
-    MEMORY_BUSY,
-    FAN_SPEED,
-    EDGE_TEMP_CURRENT,
-    JUNCTION_TEMP_CURRENT,
-    MEMORY_TEMP_CURRENT,
-    HBM_0_TEMP_CURRENT,
-    HBM_1_TEMP_CURRENT,
-    HBM_2_TEMP_CURRENT,
-    HBM_3_TEMP_CURRENT,
-    VDDGFX_VOLT_CURRENT,
-    VDDGFX_VOLT_AVERAGE,
-    DEVICE_BUSY,
-};
 
 class RocmSensor
 {
 public:
-    RocmSensor(uint32_t device_index, RocmSensorType type)
+    enum class Type
+    {
+        INVALID,
+        SOCKET_POWER,
+        AVERAGE_POWER,
+        ENERGY_COUNT,
+        MEMORY_USAGE_VRAM,
+        MEMORY_USAGE_VIS_VRAM,
+        MEMORY_USAGE_GTT,
+        MEMORY_BUSY,
+        FAN_SPEED,
+        EDGE_TEMP_CURRENT,
+        JUNCTION_TEMP_CURRENT,
+        MEMORY_TEMP_CURRENT,
+        HBM_0_TEMP_CURRENT,
+        HBM_1_TEMP_CURRENT,
+        HBM_2_TEMP_CURRENT,
+        HBM_3_TEMP_CURRENT,
+        VDDGFX_VOLT_CURRENT,
+        VDDGFX_VOLT_AVERAGE,
+        DEVICE_BUSY,
+    };
+
+public:
+    RocmSensor(uint32_t device_index, Type type)
     : device_index_(device_index), type_(type)
     {
     }
 
-    static std::map<RocmSensorType, std::string> get_sensor_names()
+    static std::map<Type, std::string> get_sensor_names()
     {
         return {
-            { RocmSensorType::SOCKET_POWER, "socket_power" },
-            { RocmSensorType::AVERAGE_POWER, "average_power" },
-            { RocmSensorType::ENERGY_COUNT, "energy_count" },
-            { RocmSensorType::MEMORY_USAGE_VRAM, "memory_usage_vram" },
-            { RocmSensorType::MEMORY_USAGE_VIS_VRAM, "memory_usage_vis_vram" },
-            { RocmSensorType::MEMORY_USAGE_GTT, "memory_usage_gtt" },
-            { RocmSensorType::MEMORY_BUSY, "memory_busy" },
-            { RocmSensorType::FAN_SPEED, "fan_speed" },
-            { RocmSensorType::EDGE_TEMP_CURRENT, "edge_temp_current" },
-            { RocmSensorType::JUNCTION_TEMP_CURRENT, "junction_temp_current" },
-            { RocmSensorType::MEMORY_TEMP_CURRENT, "memory_temp_current" },
-            { RocmSensorType::HBM_0_TEMP_CURRENT, "hbm0_temp_current" },
-            { RocmSensorType::HBM_1_TEMP_CURRENT, "hbm1_temp_current" },
-            { RocmSensorType::HBM_2_TEMP_CURRENT, "hbm2_temp_current" },
-            { RocmSensorType::HBM_3_TEMP_CURRENT, "hbm3_temp_current" },
-            { RocmSensorType::VDDGFX_VOLT_CURRENT, "vddgfx_volt_current" },
-            { RocmSensorType::VDDGFX_VOLT_AVERAGE, "vddgfx_volt_average" },
-            { RocmSensorType::DEVICE_BUSY, "device_busy" },
+            { Type::SOCKET_POWER, "socket_power" },
+            { Type::AVERAGE_POWER, "average_power" },
+            { Type::ENERGY_COUNT, "energy_count" },
+            { Type::MEMORY_USAGE_VRAM, "memory_usage_vram" },
+            { Type::MEMORY_USAGE_VIS_VRAM, "memory_usage_vis_vram" },
+            { Type::MEMORY_USAGE_GTT, "memory_usage_gtt" },
+            { Type::MEMORY_BUSY, "memory_busy" },
+            { Type::FAN_SPEED, "fan_speed" },
+            { Type::EDGE_TEMP_CURRENT, "edge_temp_current" },
+            { Type::JUNCTION_TEMP_CURRENT, "junction_temp_current" },
+            { Type::MEMORY_TEMP_CURRENT, "memory_temp_current" },
+            { Type::HBM_0_TEMP_CURRENT, "hbm0_temp_current" },
+            { Type::HBM_1_TEMP_CURRENT, "hbm1_temp_current" },
+            { Type::HBM_2_TEMP_CURRENT, "hbm2_temp_current" },
+            { Type::HBM_3_TEMP_CURRENT, "hbm3_temp_current" },
+            { Type::VDDGFX_VOLT_CURRENT, "vddgfx_volt_current" },
+            { Type::VDDGFX_VOLT_AVERAGE, "vddgfx_volt_average" },
+            { Type::DEVICE_BUSY, "device_busy" },
         };
     }
 
@@ -74,41 +76,41 @@ public:
     {
         switch (type_)
         {
-        case RocmSensorType::SOCKET_POWER:
+        case Type::SOCKET_POWER:
             return "Socket Power";
-        case RocmSensorType::AVERAGE_POWER:
+        case Type::AVERAGE_POWER:
             return "Average Power";
-        case RocmSensorType::ENERGY_COUNT:
+        case Type::ENERGY_COUNT:
             return "Energy Count";
-        case RocmSensorType::MEMORY_USAGE_VRAM:
+        case Type::MEMORY_USAGE_VRAM:
             return "Memory Usage (VRAM)";
-        case RocmSensorType::MEMORY_USAGE_VIS_VRAM:
+        case Type::MEMORY_USAGE_VIS_VRAM:
             return "Memory Usage (Visible VRAM)";
-        case RocmSensorType::MEMORY_USAGE_GTT:
+        case Type::MEMORY_USAGE_GTT:
             return "Memory Usage (GTT)";
-        case RocmSensorType::MEMORY_BUSY:
+        case Type::MEMORY_BUSY:
             return "Memory Busy Percent";
-        case RocmSensorType::FAN_SPEED:
+        case Type::FAN_SPEED:
             return "Fan Speed";
-        case RocmSensorType::EDGE_TEMP_CURRENT:
+        case Type::EDGE_TEMP_CURRENT:
             return "Current Edge GPU temperature";
-        case RocmSensorType::JUNCTION_TEMP_CURRENT:
+        case Type::JUNCTION_TEMP_CURRENT:
             return "Current junction/hotspot temperature";
-        case RocmSensorType::MEMORY_TEMP_CURRENT:
+        case Type::MEMORY_TEMP_CURRENT:
             return "Current GPU memory temperature";
-        case RocmSensorType::HBM_0_TEMP_CURRENT:
+        case Type::HBM_0_TEMP_CURRENT:
             return "Current HBM0 temperature";
-        case RocmSensorType::HBM_1_TEMP_CURRENT:
+        case Type::HBM_1_TEMP_CURRENT:
             return "Current HBM1 temperature";
-        case RocmSensorType::HBM_2_TEMP_CURRENT:
+        case Type::HBM_2_TEMP_CURRENT:
             return "Current HBM2 temperature";
-        case RocmSensorType::HBM_3_TEMP_CURRENT:
+        case Type::HBM_3_TEMP_CURRENT:
             return "Current HBM3 temperature";
-        case RocmSensorType::VDDGFX_VOLT_CURRENT:
+        case Type::VDDGFX_VOLT_CURRENT:
             return "Current Vdd_gfx voltage";
-        case RocmSensorType::VDDGFX_VOLT_AVERAGE:
+        case Type::VDDGFX_VOLT_AVERAGE:
             return "Average Vdd_gfx voltage";
-        case RocmSensorType::DEVICE_BUSY:
+        case Type::DEVICE_BUSY:
             return "Percentage of time device is busy";
         }
         return "unknown";
@@ -118,32 +120,32 @@ public:
     {
         switch (type_)
         {
-        case RocmSensorType::SOCKET_POWER:
+        case Type::SOCKET_POWER:
             return "W";
-        case RocmSensorType::AVERAGE_POWER:
+        case Type::AVERAGE_POWER:
             return "W";
-        case RocmSensorType::ENERGY_COUNT:
+        case Type::ENERGY_COUNT:
             return "J";
-        case RocmSensorType::MEMORY_USAGE_VRAM:
-        case RocmSensorType::MEMORY_USAGE_VIS_VRAM:
-        case RocmSensorType::MEMORY_USAGE_GTT:
+        case Type::MEMORY_USAGE_VRAM:
+        case Type::MEMORY_USAGE_VIS_VRAM:
+        case Type::MEMORY_USAGE_GTT:
             // TODO: Real Unit
             return "B";
-        case RocmSensorType::MEMORY_BUSY:
-        case RocmSensorType::DEVICE_BUSY:
+        case Type::MEMORY_BUSY:
+        case Type::DEVICE_BUSY:
             return "%";
-        case RocmSensorType::FAN_SPEED:
+        case Type::FAN_SPEED:
             return "rpm";
-        case RocmSensorType::EDGE_TEMP_CURRENT:
-        case RocmSensorType::JUNCTION_TEMP_CURRENT:
-        case RocmSensorType::MEMORY_TEMP_CURRENT:
-        case RocmSensorType::HBM_0_TEMP_CURRENT:
-        case RocmSensorType::HBM_1_TEMP_CURRENT:
-        case RocmSensorType::HBM_2_TEMP_CURRENT:
-        case RocmSensorType::HBM_3_TEMP_CURRENT:
+        case Type::EDGE_TEMP_CURRENT:
+        case Type::JUNCTION_TEMP_CURRENT:
+        case Type::MEMORY_TEMP_CURRENT:
+        case Type::HBM_0_TEMP_CURRENT:
+        case Type::HBM_1_TEMP_CURRENT:
+        case Type::HBM_2_TEMP_CURRENT:
+        case Type::HBM_3_TEMP_CURRENT:
             return "C";
-        case RocmSensorType::VDDGFX_VOLT_CURRENT:
-        case RocmSensorType::VDDGFX_VOLT_AVERAGE:
+        case Type::VDDGFX_VOLT_CURRENT:
+        case Type::VDDGFX_VOLT_AVERAGE:
             return "V";
         }
         return "#";
@@ -158,67 +160,67 @@ public:
         rsmi_status_t ret;
         switch (type_)
         {
-        case RocmSensorType::SOCKET_POWER:
+        case Type::SOCKET_POWER:
             ret = rsmi_dev_current_socket_power_get(device_index_, &val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::AVERAGE_POWER:
+        case Type::AVERAGE_POWER:
             ret = rsmi_dev_power_ave_get(device_index_, 0, &val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::ENERGY_COUNT:
+        case Type::ENERGY_COUNT:
             ret = rsmi_dev_energy_count_get(device_index_, &val, &counter_resolution, &timestamp);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::MEMORY_USAGE_VRAM:
+        case Type::MEMORY_USAGE_VRAM:
             ret = rsmi_dev_memory_usage_get(device_index_, RSMI_MEM_TYPE_VRAM, &val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::MEMORY_USAGE_VIS_VRAM:
+        case Type::MEMORY_USAGE_VIS_VRAM:
             ret = rsmi_dev_memory_usage_get(device_index_, RSMI_MEM_TYPE_VIS_VRAM, &val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::MEMORY_USAGE_GTT:
+        case Type::MEMORY_USAGE_GTT:
             ret = rsmi_dev_memory_usage_get(device_index_, RSMI_MEM_TYPE_GTT, &val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::MEMORY_BUSY:
+        case Type::MEMORY_BUSY:
             ret = rsmi_dev_memory_busy_percent_get(device_index_, &val32);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::FAN_SPEED:
+        case Type::FAN_SPEED:
             ret = rsmi_dev_fan_rpms_get(device_index_, 0, (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::EDGE_TEMP_CURRENT:
+        case Type::EDGE_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_EDGE, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::JUNCTION_TEMP_CURRENT:
+        case Type::JUNCTION_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_JUNCTION,
                                            RSMI_TEMP_CURRENT, (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::MEMORY_TEMP_CURRENT:
+        case Type::MEMORY_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_MEMORY, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::HBM_0_TEMP_CURRENT:
+        case Type::HBM_0_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_0, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::HBM_1_TEMP_CURRENT:
+        case Type::HBM_1_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_1, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::HBM_2_TEMP_CURRENT:
+        case Type::HBM_2_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_2, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::HBM_3_TEMP_CURRENT:
+        case Type::HBM_3_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_3, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::VDDGFX_VOLT_CURRENT:
+        case Type::VDDGFX_VOLT_CURRENT:
             ret = rsmi_dev_volt_metric_get(device_index_, RSMI_VOLT_TYPE_VDDGFX, RSMI_VOLT_CURRENT,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::VDDGFX_VOLT_AVERAGE:
+        case Type::VDDGFX_VOLT_AVERAGE:
             ret = rsmi_dev_volt_metric_get(device_index_, RSMI_VOLT_TYPE_VDDGFX, RSMI_VOLT_AVERAGE,
                                            (int64_t*)&val);
             return (ret == RSMI_STATUS_SUCCESS);
-        case RocmSensorType::DEVICE_BUSY:
+        case Type::DEVICE_BUSY:
             ret = rsmi_dev_busy_percent_get(device_index_, &val32);
             return (ret == RSMI_STATUS_SUCCESS);
         default:
@@ -236,67 +238,67 @@ public:
         rsmi_status_t ret;
         switch (type_)
         {
-        case RocmSensorType::SOCKET_POWER:
+        case Type::SOCKET_POWER:
             ret = rsmi_dev_current_socket_power_get(device_index_, &val);
             return val * 0.000001;
-        case RocmSensorType::AVERAGE_POWER:
+        case Type::AVERAGE_POWER:
             ret = rsmi_dev_power_ave_get(device_index_, 0, &val);
             return val * 0.000001;
-        case RocmSensorType::ENERGY_COUNT:
+        case Type::ENERGY_COUNT:
             ret = rsmi_dev_energy_count_get(device_index_, &val, &counter_resolution, &timestamp);
             return val * counter_resolution * 0.000001;
-        case RocmSensorType::MEMORY_USAGE_VRAM:
+        case Type::MEMORY_USAGE_VRAM:
             ret = rsmi_dev_memory_usage_get(device_index_, RSMI_MEM_TYPE_VRAM, &val);
             return val;
-        case RocmSensorType::MEMORY_USAGE_VIS_VRAM:
+        case Type::MEMORY_USAGE_VIS_VRAM:
             ret = rsmi_dev_memory_usage_get(device_index_, RSMI_MEM_TYPE_VIS_VRAM, &val);
             return val;
-        case RocmSensorType::MEMORY_USAGE_GTT:
+        case Type::MEMORY_USAGE_GTT:
             ret = rsmi_dev_memory_usage_get(device_index_, RSMI_MEM_TYPE_GTT, &val);
             return val;
-        case RocmSensorType::MEMORY_BUSY:
+        case Type::MEMORY_BUSY:
             ret = rsmi_dev_memory_busy_percent_get(device_index_, &val32);
             return val32;
-        case RocmSensorType::FAN_SPEED:
+        case Type::FAN_SPEED:
             ret = rsmi_dev_fan_rpms_get(device_index_, 0, (int64_t*)&val);
             return val;
-        case RocmSensorType::EDGE_TEMP_CURRENT:
+        case Type::EDGE_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_EDGE, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::JUNCTION_TEMP_CURRENT:
+        case Type::JUNCTION_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_JUNCTION,
                                            RSMI_TEMP_CURRENT, (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::MEMORY_TEMP_CURRENT:
+        case Type::MEMORY_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_MEMORY, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::HBM_0_TEMP_CURRENT:
+        case Type::HBM_0_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_0, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::HBM_1_TEMP_CURRENT:
+        case Type::HBM_1_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_1, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::HBM_2_TEMP_CURRENT:
+        case Type::HBM_2_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_2, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::HBM_3_TEMP_CURRENT:
+        case Type::HBM_3_TEMP_CURRENT:
             ret = rsmi_dev_temp_metric_get(device_index_, RSMI_TEMP_TYPE_HBM_3, RSMI_TEMP_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::VDDGFX_VOLT_CURRENT:
+        case Type::VDDGFX_VOLT_CURRENT:
             ret = rsmi_dev_volt_metric_get(device_index_, RSMI_VOLT_TYPE_VDDGFX, RSMI_VOLT_CURRENT,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::VDDGFX_VOLT_AVERAGE:
+        case Type::VDDGFX_VOLT_AVERAGE:
             ret = rsmi_dev_volt_metric_get(device_index_, RSMI_VOLT_TYPE_VDDGFX, RSMI_VOLT_AVERAGE,
                                            (int64_t*)&val);
             return val * 0.001;
-        case RocmSensorType::DEVICE_BUSY:
+        case Type::DEVICE_BUSY:
             ret = rsmi_dev_busy_percent_get(device_index_, &val32);
             return val32;
         default:
@@ -315,5 +317,5 @@ public:
 
 private:
     uint32_t device_index_;
-    RocmSensorType type_;
+    Type type_;
 };
